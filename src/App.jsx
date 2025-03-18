1a8ea17d34645c4a613923bcb7c5e060cebe0682
Partha Sen <senpartho15@gmail.com>
2025-03-19 02:13:12 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Contact from './sections/Contact/Contact'
 import Hero from './sections/Hero/Hero'
 import Projects from './sections/Projects/Projects' 
 import Skills from './sections/Skills/Skills'
+import Footer from './sections/Footer/Footer'
 function App() {
   
 
@@ -13,6 +14,7 @@ function App() {
     <Projects />
     <Skills/>
     <Contact/>
+    <Footer/>
     </>
   )
 }
